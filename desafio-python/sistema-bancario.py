69fe929ffbc4ff3aebe45ba04e98e13ee2e26ccf
nadinecarvalho <nadsmcarvalho@gmail.com>
2025-02-26 18:09:23 -0300

    arquivo do desafio bancário

---
+++ desafio-python/sistema-bancario.py
@@ -0,0 +1,67 @@
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+limite_saques = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= limite_saques
+
+        if excedeu_saldo:
+            print("Operação falhou! Saldo insuficiente.")
+        
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Limite de saques atingido.")
+        
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n========== EXTRATO ==========\n")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"Saldo: R$ {saldo:.2f}")
+        print(f"Limite: R$ {limite:.2f}")
+        print(f"Saques realizados: {numero_saques}/{limite_saques}")
+        print("\n=============================\n")
+
+    elif opcao == "0":
+        print("Saindo...")
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
